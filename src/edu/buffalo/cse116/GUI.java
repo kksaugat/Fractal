3d3ec2c15dd25a5df016c93a94610c8a59ab2532
declan_sidoti <declansidoti@gmail.com>
2017-04-13 21:11:08 -0400

    changing esc time/dist

--- src/edu/buffalo/cse116/GUI.java
+++ src/edu/buffalo/cse116/GUI.java
@@ -43,6 +43,7 @@ private static JButton button;
 private static int [][]_array;
 private static IndexColorModel _model = ColorModelFactory.createBluesColorModel(18);	
 public GUI(){
+	
 	_fractalPanel = new FractalPanel();
 	_main = new Main();
 	_array = _main.setArrMultiBrot();
@@ -54,16 +55,16 @@ public GUI(){
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	
 	MenuBar = new JMenuBar();
-	 frame.setJMenuBar(MenuBar);
-	  Fractals = new JMenu("Fractals");
-	  ColorSchemes = new JMenu("ColorSchemes");
-	  EscDistance = new JMenu("EscDistance");
+	frame.setJMenuBar(MenuBar);
+	Fractals = new JMenu("Fractals");
+	ColorSchemes = new JMenu("ColorSchemes");
+	EscDistance = new JMenu("EscDistance");
 	 
-	  textField = new JFormattedTextField();
-	  File = new JMenu("File");
-	  button = new JButton("change");
+	textField = new JFormattedTextField();
+	File = new JMenu("File");
+	button = new JButton("Enter");
 	  
-	 MenuBar.add(Fractals);
+	MenuBar.add(Fractals);
 	MenuBar.add(File);
 	MenuBar.add(ColorSchemes);
 	MenuBar.add(EscDistance);
@@ -71,18 +72,18 @@ public GUI(){
 	EscDistance.add(textField);
 	EscDistance.add(button);
 
-	 MandelBrot = new JMenuItem("MandelBrot");
-	 Julia = new JMenuItem("Julia");
-	 MultiBrot = new JMenuItem("MultiBrot");
-	 BurningShip = new JMenuItem("BurningShip");
-	 Fractals.add(MandelBrot);
-	 Fractals.add(MultiBrot);
-	 Fractals.add(BurningShip);
-	 Fractals.add(Julia);
-	 Rainbow = new JMenuItem("Rainbow");
-	 Blues = new JMenuItem("Blues");
-	 CottonCandy = new JMenuItem("CottonCandy");
-	 Grays = new JMenuItem("Grays");
+	MandelBrot = new JMenuItem("MandelBrot");
+	Julia = new JMenuItem("Julia");
+	MultiBrot = new JMenuItem("MultiBrot");
+	BurningShip = new JMenuItem("BurningShip");
+	Fractals.add(MandelBrot);
+	Fractals.add(MultiBrot);
+	Fractals.add(BurningShip);
+	Fractals.add(Julia);
+	Rainbow = new JMenuItem("Rainbow");
+	Blues = new JMenuItem("Blues");
+	CottonCandy = new JMenuItem("CottonCandy");
+	Grays = new JMenuItem("Grays");
 	ColorSchemes.add(Rainbow);
 	ColorSchemes.add(CottonCandy);
 	ColorSchemes.add(Grays);
@@ -96,57 +97,59 @@ public GUI(){
     CottonCandy.addActionListener(new ColorAction());
     Grays.addActionListener(new ColorAction());
     button.addActionListener(new ButtonAction());
-Exit = new JMenuItem("Exit");
-File.add(Exit);
-Exit.addActionListener(new ExitAction());
-frame.pack();
+    Exit = new JMenuItem("Exit");
+    File.add(Exit);
+    Exit.addActionListener(new ExitAction());
+    frame.pack();
 
 }
 	
- class ExitAction implements ActionListener{
-		@Override
-		public void actionPerformed (ActionEvent e){
+class ExitAction implements ActionListener{
+	@Override
+	public void actionPerformed (ActionEvent e){
 		System.exit(0);
-	}}
+	}
+}
 
 
 class ButtonAction implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e){
-		if(e.getActionCommand().equals(button)){
+		if(e.getActionCommand().equals("Enter")){
 			double dis = Double.parseDouble(textField.getText());
 			_main.setEscDist(dis);
+			
 		}
 	}
 }
-	 class FractalsAction implements ActionListener{
-@Override
-public void actionPerformed (ActionEvent e){
-			if(e.getActionCommand().equals("MultiBrot")){ 
+class FractalsAction implements ActionListener{
+	@Override
+	public void actionPerformed (ActionEvent e){
+		if(e.getActionCommand().equals("MultiBrot")){ 
 				
-				_array = _main.setArrMultiBrot(); 
-				_fractalPanel.setIndexColorModel(_model); 
-				_fractalPanel.updateImage(_array); 
-				System.out.println("MultiBrot Selected"); 
-			}
+			_array = _main.setArrMultiBrot(); 
+			_fractalPanel.setIndexColorModel(_model); 
+			_fractalPanel.updateImage(_array); 
+			System.out.println("MultiBrot Selected"); 
+		}
 			
-			if(e.getActionCommand().equals("MandelBrot")){
+		if(e.getActionCommand().equals("MandelBrot")){
 				
-				_array = _main.setArrMandelBrot();
-				_fractalPanel.setIndexColorModel(_model);
-				_fractalPanel.updateImage(_array);
-				System.out.println("MandelBrot Selected");
-			}
+			_array = _main.setArrMandelBrot();
+			_fractalPanel.setIndexColorModel(_model);
+			_fractalPanel.updateImage(_array);
+			System.out.println("MandelBrot Selected");
+		}
 			
-			if(e.getActionCommand().equals("BurningShip")){
+		if(e.getActionCommand().equals("BurningShip")){
 				
-				_array = _main.setArrBurningShip();
-				_fractalPanel.setIndexColorModel(_model);
-				_fractalPanel.updateImage(_array);
-				System.out.println("BurningShip Selected");
-			}
+			_array = _main.setArrBurningShip();
+			_fractalPanel.setIndexColorModel(_model);
+			_fractalPanel.updateImage(_array);
+			System.out.println("BurningShip Selected");
+		}
 			
-			if(e.getActionCommand().equals("Julia")){
+		if(e.getActionCommand().equals("Julia")){
 				
 				_array = _main.setArrJulia();
 				_fractalPanel.setIndexColorModel(_model);
