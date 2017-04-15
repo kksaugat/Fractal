45ce6fd7d72630281f8bdaaced2408400ad638d2
saugatkh <saugatkh@Buffalo.edu>
2017-04-15 15:36:48 -0400

    The test cases for the escapetimes which is 135.

---
+++ Main.java
@@ -0,0 +1,340 @@
+package edu.buffalo.cse116;
+
+
+
+// GENERAL IDEA:
+//Set xCalc = current point's x-coordinate
+//Set yCalc = current point's y-coordinate
+//Set dist = distance from the point (xCalc, yCalc) to (0,0) using the Pythagorean theorem
+//Set passes = 0
+//While dist ≤ 4 and passes < 255 do
+//  Update xCalc and yCalc equal using the update function defined for the fractal being generated
+//  Increase passes by 1
+//  Set dist distance from the point (xCalc, yCalc) to (0,0) using the Pythagorean theorem
+//EndWhile
+//Set the current point's escape-time equal to passes 
+
+
+//PHASE 1:
+   // CALC ESACAPE-TIME FOR EACH FRACTAL
+public class Main  {
+	
+	double xPoint;
+	double yPoint;
+	private static double distance = 2; //defaults to 2
+
+	public void setEscDist(double dist){
+		if(dist > 0){distance = dist;}
+	}
+	public int Mandelbrot(double x, double y){//Taking our coordinates as input
+		
+			double xCalc = x;   // X' 
+			double yCalc = y;   // Y'
+			double dist=Math.sqrt(Math.pow(xCalc, 2) + Math.pow(yCalc, 2));;  // Distance
+			int passes = 0; // # of passes
+		
+			while (dist <= distance && passes <255){ //Arbitrary values used for loop
+				xCalc = Math.pow(xCalc, 2) - Math.pow(yCalc, 2) + x; 
+				yCalc = 2 * xCalc * yCalc + y;
+				passes++;
+				dist = Math.sqrt(Math.pow(xCalc, 2) + Math.pow(yCalc, 2)); //Distance using pythag
+		
+			}
+			int escapeTime = passes;			
+			return escapeTime;
+		}
+//		x' = x² - y² + current point's x-coordinate
+//		y' = 2 * x * y + current point's y-coordinate
+//		   where x and y are the values of xCalc and yCalc prior to this update and x' and y' are their values after the update.
+//		X-coordinate range from -2.15 to 0.6
+//		Y-coordinate range from -1.3 to 1.3
+	
+	
+	public int Julia(double x, double y){
+		
+			double xCalc = x;   // X' 
+			double yCalc= y;   // Y'
+			double dist=Math.sqrt(Math.pow(xCalc, 2) + Math.pow(yCalc, 2));;  // Distance
+			int passes = 0; // # of passes
+		
+			while (dist <= distance && passes <255){ //Arbitrary values used for loop
+				xCalc = Math.pow(xCalc, 2) - Math.pow(yCalc, 2) - 0.72689; 
+				yCalc = 2 * xCalc * yCalc + 0.188887;
+				passes++;
+				dist = Math.sqrt(Math.pow(xCalc, 2) + Math.pow(yCalc, 2)); //Distance using pythag
+				
+			}
+			int escapeTime = passes;
+            return escapeTime;
+		}
+//		x' = x² - y² + -0.72689
+//		y' = 2 * x * y + 0.188887
+//		   where x and y are the values of xCalc and yCalc prior to this update and x' and y' are their values after the update.
+//		X-coordinate range from -1.7 to 1.7
+//		Y-coordinate range from -1.0 to 1.0
+	
+	
+	public int BurningShip(double x, double y){
+		
+			double xCalc = x;   // X' 
+			double yCalc = y;   // Y'
+			double dist=Math.sqrt(Math.pow(xCalc, 2) + Math.pow(yCalc, 2));;  // Distance
+			int passes = 0; // # of passes
+		
+			while (dist <= distance && passes <255){ //Arbitrary values used for loop
+				xCalc = Math.pow(xCalc, 2) - Math.pow(yCalc, 2) + x; 
+				yCalc = Math.abs(2 * xCalc * yCalc) + y;
+				passes++;
+				dist = Math.sqrt(Math.pow(xCalc, 2) + Math.pow(yCalc, 2)); //Distance using pythag
+				
+				
+			}
+			int escapeTime = passes;
+		    return escapeTime;
+		}
+//		x' = x² - y² + current point's x-coordinate
+//		y' = Math.abs(2 * x * y) + current point's y-coordinate
+//		   where x and y are the values of xCalc and yCalc prior to this update and x' and y' are their values after the update.
+//		X-coordinate range from -1.8 to -1.7
+//		Y-coordinate range from -0.08 to 0.025
+	
+
+	public int Multibrot(double x, double y){
+		
+			double xCalc = x;
+			double yCalc = y;
+			double dist =Math.sqrt(Math.pow(xCalc, 2) + Math.pow(yCalc, 2));;
+			int passes = 0;
+			
+			while(dist<= distance && passes <255){
+				xCalc = Math.pow(xCalc, 3) - (3 * xCalc * Math.pow(yCalc, 2)) + x;
+				yCalc = (3*Math.pow(xCalc, 2) * yCalc) - Math.pow(yCalc, 3) + y;
+				passes++;
+				dist = Math.sqrt(Math.pow(xCalc, 2)+ Math.pow(yCalc, 2));
+	
+			
+			
+			}
+			int escapeTime = passes;
+            return escapeTime;
+			
+			
+		}
+		
+		//x' = x³ - (3 * x * y²) + current point's x-coordinate
+		//y' = (3 * x² * y) - y³ + current point's y-coordinate
+		  // where x and y are the values of xCalc and yCalc prior to this update and x' and y' are their values after the update.
+	//	X-coordinate range from -1 to 1
+		//Y-coordinate range from -1.3 to 1.3
+	
+	
+	
+	
+	public int[][] setArrMultiBrot(){ //x and y values = array[x][y] and sets xPoint and yPoint in array 
+		
+			double xMin = -1.0; //ranges
+			double xMax = 1.0;
+			double yMin = -1.3;
+			double yMax = 1.3;
+		
+			double xSpacing = (xMax-xMin)/512; //sets spacing between x values
+			double ySpacing = (yMax-yMin)/512;	//sets spacing between y values
+			int y = 0;
+			int x = 0;
+			int[][] fractalFinal = new int[512][512]; //records each indicies escapeTime
+		
+				while(y < 512 ){ //goes through array 
+					x = 0;
+			
+						while(x < 512){
+							xPoint = xMin + (x * xSpacing); 
+							yPoint = yMin + (y * ySpacing);
+							fractalFinal[x][y] = Multibrot(xPoint, yPoint); //determines escape time of xPoint/yPoint
+							x++;
+			
+						}
+						y++;
+				}
+		
+		return fractalFinal;
+		
+	}
+	
+	public int[][] setArrBurningShip(){ //x and y values = array[x][y] and sets xPoint and yPoint in array 
+		
+		double xMin = -1.8; //Ranges
+		double xMax = -1.7;
+		double yMin = -.08;
+		double yMax = .025;
+	
+		double xSpacing = (xMax-xMin)/512; //sets spacing between x values
+		double ySpacing = (yMax-yMin)/512;	//sets spacing between y values
+		int y = 0;
+		int x = 0;
+		int[][] fractalFinal = new int[512][512]; //records each indicies escapeTime
+	
+			while(y < 512 ){ //goes through array 
+				x = 0;
+		
+					while(x < 512){
+						xPoint = xMin + (x * xSpacing); //starts at min range and is increased by the position in the row int[X][]*xspacing
+						yPoint = yMin + (y * ySpacing); //same as xPoint but with the y coords
+						fractalFinal[x][y] = BurningShip(xPoint, yPoint); //determines escape time of xPoint/yPoint
+						x++;
+		
+					}
+					y++; 
+			}
+	
+	return fractalFinal;
+	
+}
+	
+	public int[][] setArrJulia(){ //x and y values = array[x][y] and sets xPoint and yPoint in array 
+		
+			double xMin = -1.7;
+			double xMax = 1.7;		//Sets the ranges for x
+			double yMin = -1.0;		//sets the ranges for y
+			double yMax = 1.0;
+	
+			double xSpacing = (xMax-xMin)/512; //sets spacing between x values
+			double ySpacing = (yMax-yMin)/512;	//sets spacing between y values
+			int y = 0;
+			int x = 0;
+			int[][] fractalFinal = new int[512][512]; //records each indicies escapeTime
+	
+				while(y < 512 ){ //goes through array 
+					
+					x = 0;
+		
+						while(x < 512){
+							xPoint = xMin + (x * xSpacing); 
+							yPoint = yMin + (y * ySpacing);
+							fractalFinal[x][y] = Julia(xPoint, yPoint); //determines escape time of xPoint/yPoint
+							x++;
+						}
+						
+					y++;
+					
+				}
+	
+	return fractalFinal;
+	
+}
+	
+	public int[][] setArrMandelBrot(){ //x and y values = array[x][y] and sets xPoint and yPoint in array 
+		
+			double xMin = -2.15;
+			double xMax = .6;
+			double yMin = -1.3;
+			double yMax = 1.3;
+	
+			double xSpacing = (xMax-xMin)/512; //sets spacing between x values
+			double ySpacing = (yMax-yMin)/512;	//sets spacing between y values
+			int y = 0;
+			int x = 0;
+			int[][] fractalFinal = new int[512][512]; //records each indicies escapeTime
+	
+				while(y < 512 ){ //goes through array 
+					x = 0;
+		
+						while(x < 512){
+							xPoint = xMin + (x * xSpacing); 
+							yPoint = yMin + (y * ySpacing);
+							fractalFinal[x][y] = Mandelbrot(xPoint, yPoint); //determines escape time of xPoint/yPoint
+							x++;
+						}
+						
+					y++;
+					
+				}
+	
+	return fractalFinal;
+	
+}
+	
+	public int[][] translateRow(int[][] someArray, int rowToCopy, int newXcoord){ 
+		
+		int[][] retVal = someArray;
+		
+			for(int cursor = 0; cursor < 512; cursor++){
+				retVal[newXcoord][cursor] = someArray[rowToCopy][cursor];
+			}
+		
+		return retVal;
+		
+	}
+	
+	public int[][] translateColumn(int[][] someArray, int columnToCopy, int newYcoord){
+		
+		int[][] retVal = someArray;
+		
+			for(int cursor = 0; cursor < 512; cursor++){
+				retVal[cursor][newYcoord] = someArray[cursor][columnToCopy];
+			}
+		
+		return retVal;
+		
+	}	
+	
+// takes x-coordinate and y-coordinate as an argument
+// returns an int of the escape time calculated from the new maximum escape time	
+	 
+	public int newescapeDistanceMandelBrot(double x, double y){
+		Main mn = new Main();
+		  
+		double dist=0;  
+		int iteration = 0; 
+	
+		while (dist <= distance && iteration <135){ 
+			mn.Mandelbrot(x, y);
+		iteration++;	
+		}
+		int escapeTime =iteration;			
+		return escapeTime;
+	}
+	public int newescapeDistanceJuila(double x, double y){
+		Main mn = new Main();
+		double dist=0; 
+		int iteration = 0; 
+	
+		while (dist <= distance && iteration <135){ 
+			mn.Julia(x, y);
+		iteration++;
+		}
+		int escapeTime = iteration;			
+		return escapeTime;
+	}
+
+public int newescapeDistanceMultiBrot(double x, double y){
+	Main mn = new Main();
+ 	double dist=0;  
+	int iteration = 0; 
+
+	while(dist<= distance && iteration <135){
+		mn.Multibrot(x, y);
+iteration++;
+	}
+	int escapeTime = iteration;			
+	return escapeTime;
+}
+
+public int newescapeDistanceBurningShip(double x, double y){
+	Main mn = new Main();
+	double dist=0;  
+	int iteration = 0; 
+
+	while (dist <= distance && iteration <135){ 
+	 mn.BurningShip(x, y);
+	iteration++;		
+	}
+	int escapeTime = iteration;			
+	return escapeTime;
+}
+}
+
+
+		
+	
+	
+
---
+++ MainTest.java
@@ -0,0 +1,315 @@
+package edu.buffalo.cse116;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+public class MainTest {
+		@Test
+		public void MandelbrotTimeExceedsTest() {
+		Main mn = new Main();
+		int coordinates = mn.Mandelbrot(0.5946289062500001, 1.2949218750000122);// x and y coordinates outside the range
+		assertEquals(3,coordinates);// should return 1 if escape time for a coordinate whose distance from the origin  exceeds the escape distance
+		}
+
+
+
+		@Test
+		public void JuliaimeExceedsTestTest() {
+		Main mn = new Main();
+		int coordinates = mn.Julia(1.6933593749999853, 0.9765625);// x and y coordinates outside the range
+		assertEquals(1,coordinates);;// should return 1 if escape time for a coordinate whose distance from the origin  exceeds the escape distance
+		}
+
+
+
+		@Test
+		public void MultibrotimeExceedsTestTest() {
+		Main mn = new Main();
+		int coordinates = mn.Multibrot(0.9921875, 1.05625);// x and y coordinates outside the range
+		assertEquals(1,coordinates);// should return 1 if escape time for a coordinate whose distance from the origin  exceeds the escape distance
+		}
+
+		@Test
+		public void Mandelbrottest() {
+		Main mn = new Main();
+		int coordinates = mn.Mandelbrot(0.3207031250000001, -0.07109374999999386);// x and y coordinates within the range
+		assertEquals(255,coordinates); // should return 255 if escape time for a coordinate whose distance from the origin never exceeds the escape distance
+		}
+
+		@Test
+		public void Juliatest() {
+		Main mn = new Main();
+		int coordinates = mn.Julia(1.0492187499999897, -0.234375);// x and y coordinates within the range
+		assertEquals(255,coordinates);// should return 255 if escape time for a coordinate whose distance from the origin never exceeds the escape distance
+		}
+
+
+	@Test
+		public void BurningShiptest() {
+		Main mn = new Main();
+		int coordinates = mn.BurningShip(-1.7443359374999874, -0.017451171875000338);// x and y coordinates within the range
+		assertEquals(69,coordinates); // should return 255 if escape time for a coordinate whose distance from the origin never exceeds the escape distance
+		}
+
+		@Test
+		public void Multibrottest() {
+		Main mn = new Main();
+		int coordinates = mn.Multibrot(0.5859375, 0.24375000000000108);// x and y coordinates within the range
+		assertEquals(255,coordinates);// should return 255 if escape time for a coordinate whose distance from the origin never exceeds the escape distance
+		}
+
+		@Test
+		public void burningShipPixelTest(){
+			Main mn = new Main();
+			int[][] arr = mn.setArrBurningShip();
+			int x= 0;
+			int y= 0;
+
+			while(y<512){//While loop goes through burningShips int[][] and makes sure nothing equals 1||0
+				x = 0;
+				while(x < 512){
+					int escapeTime = arr[x][y];
+					assertFalse((escapeTime == 1 ||escapeTime == 0)); //makes sure escapeTime ==1 || ==0 is false
+					x++;
+				}
+				y++;
+			}
+		}
+
+		@Test
+		public void MandelBrotSize(){
+			Main mn = new Main();
+			int[][] arr = mn.setArrMandelBrot();
+			int x= 0;
+			int y= 0;
+			int xCounter = 0; //should be equal to 512*512 since it goes through 512 rows with 512 columns each
+			int yCounter = 0; //should return 512
+
+			while(y<512){
+				x = 0;
+				while(x < 512){
+					int escapeTime = arr[x][y]; // makes sure nothing goes out of bounds
+					xCounter++; //counts each time x is increased
+					x++;
+				}
+				yCounter++;//counts each time y is increased
+				y++;
+			}
+			assertEquals(262144, xCounter); //xCounter == 262144
+			assertEquals(512, yCounter);	//yCounter == 512
+		}
+
+		@Test
+		public void JuliaSize(){
+			Main mn = new Main();
+			int[][] arr = mn.setArrJulia();
+			int x= 0;
+			int y= 0;
+			int xCounter = 0; //should be equal to 512*512 since it goes through 512 rows with 512 columns each
+			int yCounter = 0; //should return 512
+
+			while(y<512){
+				x = 0;
+				while(x < 512){
+					int escapeTime = arr[x][y]; //makes sure nothing goes out of bounds
+					xCounter++;//counts each time x is increased
+					x++;
+				}
+				yCounter++;//counts each time y is increased
+				y++;
+			}
+			assertEquals(262144, xCounter);
+			assertEquals(512, yCounter);
+		}
+
+		@Test
+		public void BurningShipSize(){
+			Main mn = new Main();
+			int[][] arr = mn.setArrBurningShip();
+			int x= 0;
+			int y= 0;
+			int xCounter = 0; //should be equal to 512*512 since it goes through 512 rows with 512 columns each
+			int yCounter = 0; //should return 512
+
+			while(y<512){
+				x = 0;
+				while(x < 512){
+					int escapeTime = arr[x][y]; //makes sure nothing goes out of bounds
+					xCounter++; //counts each time x is increased
+					x++;
+				}
+				yCounter++; //counts each time y is increased
+				y++;
+			}
+			assertEquals(262144, xCounter);
+			assertEquals(512, yCounter);
+		}
+
+		@Test
+		public void MultBrotSize(){
+			Main mn = new Main();
+			int[][] arr = mn.setArrMultiBrot();
+			int x= 0;
+			int y= 0;
+			int xCounter = 0; //should be equal to 512*512 since it goes through 512 rows with 512 columns each
+			int yCounter = 0; //should return 512
+
+			while(y<512){
+				x = 0;
+				while(x < 512){
+					int escapeTime = arr[x][y]; //makes sure x/y doesnt go past 512 and throws an outOfBounds execption if it does
+					xCounter++;
+					x++;
+				}
+				yCounter++;
+				y++;
+			}
+			assertEquals(262144, xCounter);
+			assertEquals(512, yCounter);
+	}
+		
+		@Test
+		public void xTranslateMultiBrot(){
+			Main m = new Main();
+			int rowToTranslate = 0; //row we want to copy and move
+			int xCoord = 1; //row that we want to copy to 
+			int[][] array = m.setArrMultiBrot(); //populates a 2d Array
+			int[][] arrayAfterTranslate = m.translateRow(array, rowToTranslate, xCoord);  //calls translateRow();
+				
+				for(int y = 0; y < 512; y++){	//loops through all coordinates at xCoord
+					int rowOriginal = array[rowToTranslate][y];  //
+					int rowNew = arrayAfterTranslate[xCoord][y];
+					assertEquals(rowOriginal, rowNew);
+				}
+		}
+		@Test
+		public void yTranslateMultiBrot(){
+			Main m = new Main();
+			int columnToCopy = 0; //row we want to copy and move
+			int yCoord = 30; //row that we want to copy to 
+			int[][] array = m.setArrMultiBrot(); //populates a 2d Array
+			int[][] arrayAfterTranslate = m.translateColumn(array, columnToCopy, yCoord);  //calls translateColumn();
+				
+				for(int x = 0; x < 512; x++){ //loops through all 512 X values at yCoord
+					int columnOriginal = array[x][columnToCopy]; 
+					int columnNew = arrayAfterTranslate[x][yCoord]; 
+					assertEquals(columnOriginal, columnNew); //makes sure each column is equal at the same x value
+				}
+		}
+		@Test
+		public void xTranslateBurningShip(){
+			Main m = new Main();
+			int rowToTranslate = 1; //row we want to copy and move
+			int xCoord = 421; //row that we want to copy to 
+			int[][] array = m.setArrBurningShip(); //populates a 2d Array
+			int[][] arrayAfterTranslate = m.translateRow(array, rowToTranslate, xCoord);  //calls translateRow();
+				
+				for(int y = 0; y < 512; y++){	//loops through all 512 Y values at xCoord
+					int rowOriginal = array[rowToTranslate][y];  //
+					int rowNew = arrayAfterTranslate[xCoord][y];
+					assertEquals(rowOriginal, rowNew);
+				}
+		}
+		@Test
+		public void yTranslateBurningShip(){
+			Main m = new Main();
+			int columnToCopy = 0; //row we want to copy and move
+			int yCoord = 91; //row that we want to copy to 
+			int[][] array = m.setArrBurningShip(); //populates a 2d Array
+			int[][] arrayAfterTranslate = m.translateColumn(array, columnToCopy, yCoord);  //calls translateColumn();
+				
+				for(int x = 0; x < 512; x++){ //loops through all 512 X values at yCoord
+					int columnOriginal = array[x][columnToCopy]; 
+					int columnNew = arrayAfterTranslate[x][yCoord]; 
+					assertEquals(columnOriginal, columnNew); //makes sure each column is equal at the same x value
+				}
+		}
+		@Test
+		public void xTranslateJulia(){
+			Main m = new Main();
+			int rowToTranslate = 3; //row we want to copy and move
+			int xCoord = 92; //row that we want to copy to 
+			int[][] array = m.setArrJulia(); //populates a 2d Array
+			int[][] arrayAfterTranslate = m.translateRow(array, rowToTranslate, xCoord);  //calls translateRow();
+				
+				for(int y = 0; y < 512; y++){	//loops through all 512 Y values at xCoord
+					int rowOriginal = array[rowToTranslate][y];  //
+					int rowNew = arrayAfterTranslate[xCoord][y];
+					assertEquals(rowOriginal, rowNew);
+				}
+		}
+		@Test
+		public void yTranslateJulia(){
+			Main m = new Main();
+			int columnToCopy = 0; //row we want to copy and move
+			int yCoord = 30; //row that we want to copy to 
+			int[][] array = m.setArrJulia(); //populates a 2d Array
+			int[][] arrayAfterTranslate = m.translateColumn(array, columnToCopy, yCoord);  //calls translateColumn();
+				
+				for(int x = 0; x < 512; x++){ //loops through all 512 X values at yCoord
+					int columnOriginal = array[x][columnToCopy]; 
+					int columnNew = arrayAfterTranslate[x][yCoord]; 
+					assertEquals(columnOriginal, columnNew); //makes sure each column is equal at the same x value
+				}
+		}
+		@Test
+		public void xTranslateMadelBrot(){
+			Main m = new Main();
+			int rowToTranslate = 511; //row we want to copy and move
+			int xCoord = 3; //row that we want to copy to 
+			int[][] array = m.setArrBurningShip(); //populates a 2d Array
+			int[][] arrayAfterTranslate = m.translateRow(array, rowToTranslate, xCoord);  //calls translateRow();
+				
+				for(int y = 0; y < 512; y++){	//loops through all 512 Y values at xCoord
+					int rowOriginal = array[rowToTranslate][y];  //
+					int rowNew = arrayAfterTranslate[xCoord][y];
+					assertEquals(rowOriginal, rowNew);
+				}
+		}
+		@Test
+		public void yTranslateMadelBrot(){
+			Main m = new Main();
+			int columnToCopy = 44; //row we want to copy and move
+			int yCoord = 122; //row that we want to copy to 
+			int[][] array = m.setArrBurningShip(); //populates a 2d Array
+			int[][] arrayAfterTranslate = m.translateColumn(array, columnToCopy, yCoord);  //calls translateColumn();
+				
+				for(int x = 0; x < 512; x++){ //loops through all 512 X values at yCoord
+					int columnOriginal = array[x][columnToCopy]; 
+					int columnNew = arrayAfterTranslate[x][yCoord]; 
+					assertEquals(columnOriginal, columnNew); //makes sure each column is equal at the same x value
+				}
+		}
+	
+		//When maximum escape time is set to 135 and distance to 2
+	@Test
+	public void newMandelbrotTest (){
+	 Main mn = new Main();
+	int result = (mn.newescapeDistanceMandelBrot(0.3207031250000001, -0.07109374999999386));
+	assertEquals(135,result);// should return the maximum escape time which is 135
+	}
+
+	public void JuliaSet (){
+    Main mn = new Main();
+	int result = (mn.newescapeDistanceJuila(1.0492187499999897, -0.234375));
+	assertEquals(135,result);// should return the maximum escape time which is 135
+	}
+	
+	
+	public void BurningShipTest (){
+    Main mn = new Main();
+	int result = (mn.newescapeDistanceBurningShip(-1.7443359374999874, -0.017451171875000338));
+	assertEquals(135,result);// should return the maximum escape time which is 135
+	}
+	
+	
+	public void MultiBrotTest(){
+    Main mn = new Main();
+	int result = (mn.newescapeDistanceMultiBrot(0.5859375, 0.24375000000000108));
+	assertEquals(135,result);// should return the maximum escape time which is 135
+	}
+
+}
+
+
